0579e98fbe59c6f81a275d78fb974ec178716ac5
Filipe Pires <filipepmachado1997@gmail.com>
2022-06-11 20:48:23 -0300

    Conclusão do Crud

--- crud_basico/routes/web.php
+++ crud_basico/routes/web.php
@@ -16,3 +16,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+########### OBSERVAÇÃO ###############
+/* Os nomes depois do @ são os nomes das funções no controller*/
+
+
+//Rota responsavel por mostrar o Formulário
+Route::get('/produto/novo', 'ProdutosController@create');
+
+//Rota responsavel por salvar o produto (ATENÇÃO PARA DEPOIS DO @)
+Route::post('/produto/novo', 'ProdutosController@store')->name('registrar_produto');
+
+//Rota responsavel pro trazer os inputs preenchidos 
+Route::get('/produto/ver/{id}', 'ProdutosController@show');
+
+
+Route::get('/produto/editar/{id}', 'ProdutosController@edit');
+Route::post('produto/editar/{id}', 'ProdutosController@update')->name('alterar_produto');
+
+Route::get('produto/excluir/{id}', 'ProdutosController@delete');
+
+//Metodo Destroy 
+Route::post('produto/excluir/{id}', 'ProdutosController@destroy')->name('excluir_produto');
